e1d11b6b77f03ec8945407e409e91adfcd210f52
anasirfan <39003284+anasirfan@users.noreply.github.com>
2023-02-25 03:46:12 +0500

    deploy

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ export class NavBar extends Component {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to={process.env.PUBLIC_URL+"/"}>Home</Link>
         </li>
         <li className="nav-item"> <Link onClick={() => this.props.setReload(true)} className="nav-link" to={process.env.PUBLIC_URL + '/business'}>Business</Link></li>
         <li className="nav-item"> <Link onClick={() => this.props.setReload(true)} className="nav-link" to={process.env.PUBLIC_URL + '/entertainment'}>Entertainment</Link></li>
